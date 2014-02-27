f1e080f4d2943cc018d0ef74394bb6507c353b51
Abhinandan <abhiin1947@gmail.com>
2014-02-27 15:13:19 +0530

    updated to latest stuff

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,12 +1,14 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, SelectField, DecimalField, IntegerField, DateField, RecaptchaField, BooleanField, TextAreaField, HiddenField
+from flask.ext.wtf import Form 
+from wtforms import TextField, TextAreaField, SelectField, DecimalField, IntegerField, DateField, BooleanField, TextAreaField, HiddenField
 from flask_security.forms import RegisterForm, LoginForm
-from flask.ext.wtf import Required, Regexp, Length, Email, URL, EqualTo, NumberRange, Recaptcha
+from wtforms.validators import Required, Regexp, Length, Email, URL, EqualTo, NumberRange
+from flask_wtf import RecaptchaField
 from flask_app import config
 
 class ExtendedRegisterForm(RegisterForm):
     first_name = TextField('First Name', [Required()])
     last_name = TextField('Last Name', [Required()])
     organization = TextField('Organisation', [Required()])
-    recaptcha = RecaptchaField([Recaptcha()])
+    recaptcha = RecaptchaField()
 
 
